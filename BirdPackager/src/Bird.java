908e34815557687879cc0d5e6894fd86abf79f94
lapualazuli <AmTAC-50>
2020-03-02 13:10:07 -0800

    Added initial files

---
+++ BirdPackager/src/Bird.java
@@ -0,0 +1,85 @@
+public class Bird {
+    private String name, descripton, finding, habitat, diet, nest, behavior, conservation, size, shape;
+
+    public Bird(){}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescripton(String descripton) {
+        this.descripton = descripton;
+    }
+
+    public void setFinding(String finding) {
+        this.finding = finding;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public void setDiet(String diet) {
+        this.diet = diet;
+    }
+
+    public void setNest(String nest) {
+        this.nest = nest;
+    }
+
+    public void setBehavior(String behavior) {
+        this.behavior = behavior;
+    }
+
+    public void setConservation(String conservation) {
+        this.conservation = conservation;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescripton() {
+        return descripton;
+    }
+
+    public String getFinding() {
+        return finding;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public String getDiet() {
+        return diet;
+    }
+
+    public String getNest() {
+        return nest;
+    }
+
+    public String getBehavior() {
+        return behavior;
+    }
+
+    public String getConservation() {
+        return conservation;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+}
