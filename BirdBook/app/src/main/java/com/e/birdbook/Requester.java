d94d14d621de167d05d2d54c9bf869370ddd345c
JFlaherty347 <JFlaherty347@gmail.com>
2020-03-01 20:24:02 -0800

    created requester class base

---
+++ BirdBook/app/src/main/java/com/e/birdbook/Requester.java
@@ -0,0 +1,29 @@
+package com.e.birdbook;
+
+public class Requester
+{
+    private Request request;
+    private Results results;
+
+    public Requester(Request request)
+    {
+        this.request = request;
+
+        if(request != null)
+        {
+            //TODO: update after discussing with designer of database accessor
+            //this.results = requester.makeQuery(this.request);
+        }
+        else
+            System.out.println("Error: Request null");
+
+        if(results != null)
+        {
+            //TODO: update after discussing with designer of packager (check bookmarks)
+            //packager.createModel(this.results);
+        }
+        else
+            System.out.println("Error: Results null");
+
+    }
+}
