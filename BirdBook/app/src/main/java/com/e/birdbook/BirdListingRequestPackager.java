38a52ef3ed39c45d9b7966b0ee9f3cef8c452e96
JFlaherty347 <JFlaherty347@gmail.com>
2020-03-04 16:48:36 -0800

    added other unit test

---
+++ BirdBook/app/src/main/java/com/e/birdbook/BirdListingRequestPackager.java
@@ -0,0 +1,15 @@
+package com.e.birdbook;
+
+import java.util.List;
+
+public class BirdListingRequestPackager
+{
+
+
+    public BirdListingRequestPackager()
+    {
+        private List<String> requestFields;
+        private String searchField;
+        private String searchValue;
+    }
+}
