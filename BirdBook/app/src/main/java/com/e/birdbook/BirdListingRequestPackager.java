f321e306a04089a57a70517be36b59796bdd25c2
JFlaherty347 <JFlaherty347@gmail.com>
2020-03-07 20:19:33 -0800

    Finished up Bird listing request packager

---
+++ BirdBook/app/src/main/java/com/e/birdbook/BirdListingRequestPackager.java
@@ -0,0 +1,20 @@
+package com.e.birdbook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BirdListingRequestPackager
+{
+
+    public static BirdInfoRequest createListingRequest()
+    {
+        List<String> requestFields = new ArrayList<String>();
+        String searchField = "all"; //not an actual field -- this is used by the database accessor to grab all entries.
+        String searchValue = "";
+
+        requestFields.add("Name");
+        requestFields.add("Image");
+
+        return new BirdInfoRequest(requestFields, searchField, searchValue);
+    }
+}
