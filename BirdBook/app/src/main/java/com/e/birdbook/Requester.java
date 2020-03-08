cb0b8f3024e9d1abfe65e2153137f56d23d4b76c
JFlaherty347 <JFlaherty347@gmail.com>
2020-03-08 12:40:18 -0700

    Made requester a static method

--- BirdBook/app/src/main/java/com/e/birdbook/Requester.java
+++ BirdBook/app/src/main/java/com/e/birdbook/Requester.java
@@ -2,28 +2,17 @@ package com.e.birdbook;
 
 public class Requester
 {
-    private Request request;
-    private Results results;
-
-    public Requester(Request request)
+    public static void Request(Request request)
     {
-        this.request = request;
-
-        if(request != null)
-        {
-            //TODO: update after discussing with designer of database accessor
-            //this.results = accessor.makeQuery(this.request);
-        }
-        else
-            System.out.println("Error: Request null");
-
-        if(results != null)
-        {
-            //TODO: update after discussing with designer of packager (check bookmarks)
-            //packager.createModel(this.results);
-        }
-        else
-            System.out.println("Error: Results null");
-
+        //TODO: update after discussing with designer of database accessor
+//        Results results = accessor.makeQuery(request);
+//
+//        if(results != null)
+//        {
+//            //TODO: update after discussing with designer of packager (check bookmarks)
+////            packager.createModel(results);
+//        }
+//        else
+//            System.out.println("Error: Results null");
     }
 }
