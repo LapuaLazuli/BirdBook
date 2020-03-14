049254189ca9da631fe916aac0a286cec5a9ee53
JFlaherty347 <JFlaherty347@gmail.com>
2020-03-13 18:16:16 -0700

    Finished bird info activity

--- BirdBook/app/src/main/java/com/e/birdbook/birdInfoActivity.java
+++ BirdBook/app/src/main/java/com/e/birdbook/birdInfoActivity.java
@@ -1,8 +1,16 @@
 package com.e.birdbook;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
 
 public class birdInfoActivity extends AppCompatActivity
 {
@@ -11,16 +19,52 @@ public class birdInfoActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bird_info_page);
 
-        //call request packager to get request
-        //send request to requester
-        //get UI friendly info
+        //get info about what page this is
+        String birdName = getIntent().getStringExtra("Bird");
 
-        loadUIfriendlyInfo();
+        //call request packager to get request
+        BirdInfoRequest req = BirdInfoRequest.createInfoRequest("name", birdName);
+
+        //send request to requester
+        Results res = Requester.request(req);
+
+        //apply UI friendly info
+        loadUIfriendlyInfo(res);
     }
 
-    public void loadUIfriendlyInfo()
+    public void loadUIfriendlyInfo(Hashtable<String, String> UIInfo)
     {
         //loop through UI elements
-            //update each element with corresponding data pieces
+        //update each element with corresponding data pieces
+
+        Enumeration<String> keysEnu = UIInfo.keys();
+        View currentUIElement;
+        String currentKey;
+        int resourceID;
+
+        while(keysEnu.hasMoreElements())
+        {
+            currentKey = keysEnu.nextElement();
+            resourceID = getResources().getIdentifier(currentKey, "id", getPackageName());
+            currentUIElement = this.findViewById(resourceID);
+
+            if(currentUIElement != null)
+            {
+                switch(currentUIElement.getClass().toString())
+                {
+                    case "TextView":
+                        TextView currentTextView = (TextView)currentUIElement;
+                        currentTextView.setText(UIInfo.get(currentKey));
+                        break;
+
+                    case "ImageView":
+                        ImageView currentImageView = (ImageView)currentUIElement;
+                        int imageId = getResources().getIdentifier(UIInfo.get(currentKey), null, getPackageName());
+                        currentImageView.setImageResource(imageId);
+                        break;
+                }
+            }
+
+        }
     }
 }
