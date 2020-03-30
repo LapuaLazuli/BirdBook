70719421fa03f0d636ac8f06bdec9d9f8e8b878f
sean <seigle15@gmail.com>
2020-03-29 21:50:15 -0700

    listing items are now clickable but buggy

--- BirdBook/app/src/main/java/com/e/birdbook/Bird_List_Activity.java
+++ BirdBook/app/src/main/java/com/e/birdbook/Bird_List_Activity.java
@@ -1,9 +1,13 @@
 package com.e.birdbook;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,19 +19,23 @@ import java.util.Enumeration;
 
 public class Bird_List_Activity extends RecyclerView.Adapter<Bird_List_Activity.ActivityViewHolder> {
     private ArrayList<UI_List_Item> birdList;
+    private Context mContext;
 
    public static class ActivityViewHolder extends RecyclerView.ViewHolder{
         public ImageView imageView;
         public TextView birdName;
+        public RelativeLayout parentLayout;
         public ActivityViewHolder(@NonNull View itemView) {
             super(itemView);
             //imageView.findViewById(R.id.ImageView);
             //imageView = imageView.findViewById(R.id.ImageView);
             birdName = itemView.findViewById(R.id.TextName);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
         }
     }
 
-    public Bird_List_Activity(ArrayList<UI_List_Item> birdList){
+    public Bird_List_Activity(Context context,  ArrayList<UI_List_Item> birdList){
+        this.mContext = context;
        getAllData();
        this.birdList = birdList;
     }
@@ -41,17 +49,28 @@ public class Bird_List_Activity extends RecyclerView.Adapter<Bird_List_Activity.
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ActivityViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ActivityViewHolder holder, final int position) {
         UI_List_Item currentItem = birdList.get(position);
        // holder.imageView.setImageResource(currentItem.getBirdImage());
         holder.birdName.setText(currentItem.getBirdName());
-
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, birdInfoActivity.class);
+                intent.putExtra("Bird", (Parcelable) birdList.get(position));
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return birdList.size();
     }
+
+
+
+
 
     public void getAllData(){
        BirdInfoRequest req = BirdListingRequestPackager.BirdListRequest();
--- BirdBook/app/src/main/java/com/e/birdbook/MainActivity.java
+++ BirdBook/app/src/main/java/com/e/birdbook/MainActivity.java
@@ -18,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<UI_List_Item> birdList = new ArrayList<>();
-        birdList.add(new UI_List_Item(R.drawable.ic_flight_black_24dp, "new bird"));
+        birdList.add(new UI_List_Item(R.drawable.ic_flight_black_24dp, "House finch"));
         birdList.add(new UI_List_Item(R.drawable.ic_android_black_24dp, "Test Name"));
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
-        birdListActivity = new Bird_List_Activity(birdList);
+        birdListActivity = new Bird_List_Activity(this, birdList);
 
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(birdListActivity);
