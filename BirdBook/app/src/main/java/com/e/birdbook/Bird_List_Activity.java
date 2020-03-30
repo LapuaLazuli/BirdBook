2fd47484af031688d623526b34242e827d149701
lapualazuli <AmTAC-50>
2020-03-29 22:53:30 -0700

    changed context to mContext

--- BirdBook/app/src/main/java/com/e/birdbook/Bird_List_Activity.java
+++ BirdBook/app/src/main/java/com/e/birdbook/Bird_List_Activity.java
@@ -2,6 +2,7 @@ package com.e.birdbook;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Parcelable;
 
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -20,7 +22,7 @@ import java.util.Enumeration;
 
 public class Bird_List_Activity extends RecyclerView.Adapter<Bird_List_Activity.ActivityViewHolder> {
     private ArrayList<UI_List_Item> birdList;
-    private Context context;
+    private Context mContext;
 
 
 
@@ -38,8 +40,9 @@ public class Bird_List_Activity extends RecyclerView.Adapter<Bird_List_Activity.
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O_MR1)
     public Bird_List_Activity(ArrayList<UI_List_Item> birdList, Context context){
-       this.context = context;
+       this.mContext = context;
 
        getAllData();
        this.birdList = birdList;
@@ -77,9 +80,10 @@ public class Bird_List_Activity extends RecyclerView.Adapter<Bird_List_Activity.
 
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O_MR1)
     public void getAllData(){
        Request req = BirdListingRequestPackager.BirdListRequest();
-       UIFriendlyInfo res = Requester.request(req, this.context);
+       UIFriendlyInfo res = Requester.request(req, this.mContext);
        if(res != null)
        {
            Dictionary<String, String> allBirds = res.getInfo();
