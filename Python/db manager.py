2512f32b5d080a701a73af5605892c6cd92508f9
Jacob Steelsmith <61163020+jacobsteelsmith279@users.noreply.github.com>
2020-04-17 16:07:45 -0700

    Add files via upload
    fixed changes to path names

--- Python/db manager.py
+++ Python/db manager.py
@@ -3,12 +3,12 @@ import sqlite3
 conn = sqlite3.connect('bird.db')
 cur = conn.cursor()
 for row in conn.execute('SELECT * FROM birds'):
-    print(row[0].lower().replace(' ', '_') + '.jpg')
+    print(row[0].lower().replace(' ', '_').replace('-', '_') + '.jpg')
     command = 'UPDATE birds set image_path = ? where name= ?'
-    data = (row[0].lower().replace(' ', '_') + '.jpg', row[0])
+    data = (row[0].lower().replace(' ', '_').replace('-', '_') + '.jpg', row[0])
     cur.execute(command, data)
     command = 'UPDATE birds set audio_path = ? where name= ?'
-    data = (row[0].lower().replace(' ', '_') + '.mp3', row[0])
+    data = (row[0].lower().replace(' ', '_').replace('-', '_') + '.mp3', row[0])
     cur.execute(command, data)
 
 conn.commit()
